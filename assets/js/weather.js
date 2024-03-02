6e577d90575ed180028b0cc0048879ba8f75437c
Christian Blanco <cblanco@fiec.espol.edu.ec>
2024-03-02 01:46:05 -0500

    Funcion loadWeekForecastData

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,8 +1,16 @@
 import { weather_data } from './data.js';
 
+   
+
+
+
+let loadDayForecastData = (a) => {
+
+
     let[guayaquil,ambato,tena]=weather_data;
 
-    let{city,date,maxtemperature,mintemperature,cloudiness,wind,rainfall,forecast_today, ...other}=tena;
+    
+    let{city,date,maxtemperature,mintemperature,cloudiness,wind,rainfall,forecast_today, ...other}=guayaquil;
     
     let[tarde,noche]=forecast_today;
     
@@ -11,8 +19,6 @@ import { weather_data } from './data.js';
     let{forecast:forecast_noche,temperature:temperature_noche,icon:icon_noche}=noche;
 
 
-
-let loadDayForecastData = () => {
 
     let ciudad = document.getElementById("city");
     ciudad.innerHTML=city;
@@ -67,7 +73,41 @@ let loadDayForecastData = () => {
     console.log(noche_icon);	
 }
 
-let loadWeekForecastData = () => {
+
+
+let loadWeekForecastData = (a) => {
+
+    let element_uls = document.getElementsByTagName("ul");
+    let ul = element_uls[0];
+
+
+    let [guayaquil,ambato,tena] = weather_data;
+
+    let {forecast_week} = guayaquil;
+
+    let [dia1,dia2,dia3,dia4,dia5,dia6,dia7]=forecast_week;
+
+    let plantilla_final="";
+
+    
+    for(let i=0; i<forecast_week.length;i++)
+    {
+        let plantilla_semanal = `<li class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg">
+        <div class="d-flex flex-column">
+          <h6 class="mb-1 text-dark font-weight-bold text-sm">${forecast_week[i].text}</h6>
+          <span class="text-xs">${forecast_week[i].date}</span>
+        </div>
+        <div class="d-flex align-items-center ">
+          <span class="font-weight-bold text-dark mx-2">${forecast_week[i].temperature.max}</span> |  <span class="text-dark mx-2">${forecast_week[i].temperature.min}</span>
+          <div class="ms-4"><i class="material-icons fs-2 me-1 rainy">${forecast_week[i].icon}</i></div>
+        </div>
+      </li> `
+      
+      plantilla_final = plantilla_final + plantilla_semanal;  
+
+      }
+
+     ul.innerHTML= plantilla_final;
 		
 }
 
