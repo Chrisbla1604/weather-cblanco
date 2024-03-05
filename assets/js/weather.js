4d197da795d2f58990021ef9073cb5d817ba46f0
Christian Blanco <cblanco@fiec.espol.edu.ec>
2024-03-04 21:05:05 -0500

    Evento DropdownMenuButton

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,16 +1,35 @@
+
+
+
 import { weather_data } from './data.js';
 
-   
+let selectedCity="ninguna";
+
+let seleccion = new Object();
 
 
-
-let loadDayForecastData = (a) => {
+let loadDayForecastData = (city_selected) => {
 
 
     let[guayaquil,ambato,tena]=weather_data;
 
+    switch (city_selected) {
+        case "guayaquil":
+          seleccion = guayaquil;
+          console.log(seleccion);
+          break;
+        case "ambato":
+          seleccion = ambato;
+          console.log(seleccion);
+          break;
+        case "tena":
+          seleccion = tena;
+          console.log(seleccion);
+          break;
+    }
+
     
-    let{city,date,maxtemperature,mintemperature,cloudiness,wind,rainfall,forecast_today, ...other}=guayaquil;
+    let{city,date,maxtemperature,mintemperature,cloudiness,wind,rainfall,forecast_today, ...other}=seleccion;
     
     let[tarde,noche]=forecast_today;
     
@@ -22,60 +41,47 @@ let loadDayForecastData = (a) => {
 
     let ciudad = document.getElementById("city");
     ciudad.innerHTML=city;
-    console.log(ciudad);
 
     let dia = document.getElementById("date");
     dia.innerHTML=date;
-    console.log(dia);
 
     let maxtemperatura = document.getElementById("maxtemperature");
     maxtemperatura.innerHTML=maxtemperature;
-    console.log(maxtemperatura);
 
     let mintemperatura = document.getElementById("mintemperature");
     mintemperatura.innerHTML=mintemperature;
-    console.log(mintemperatura);
 
     let nubosidad = document.getElementById("cloudiness");
     nubosidad.innerHTML=cloudiness;
-    console.log(nubosidad);
 
     let viento = document.getElementById("wind");
     viento.innerHTML=wind;
-    console.log(viento);
 
     let lluvia = document.getElementById("rainfall");
     lluvia.innerHTML=rainfall;
-    console.log(lluvia);
 
     let tarde_forecast = document.getElementById("late_forecast");
     tarde_forecast.innerHTML=forecast_tarde;
-    console.log(tarde_forecast);
 
     let tarde_temperature = document.getElementById("late_temperature");
     tarde_temperature.innerHTML=temperature_tarde;
-    console.log(tarde_temperature);
 
     let tarde_icon = document.getElementById("late_icon");
     tarde_icon.innerHTML=icon_tarde;
-    console.log(tarde_icon);
 
     let noche_forecast = document.getElementById("night_forecast");
     noche_forecast.innerHTML=forecast_noche;
-    console.log(noche_forecast);
 
     let noche_temperature = document.getElementById("night_temperature");
     noche_temperature.innerHTML=temperature_noche;
-    console.log(noche_temperature);
 
     let noche_icon = document.getElementById("night_icon");
-    noche_icon.innerHTML=icon_noche;
-    console.log(noche_icon);	
+    noche_icon.innerHTML=icon_noche;	
 }
 
 
 
-let loadWeekForecastData = (a) => {
+let loadWeekForecastData = (city_selected) => {
 
     let element_uls = document.getElementsByTagName("ul");
     let ul = element_uls[0];
@@ -83,7 +89,22 @@ let loadWeekForecastData = (a) => {
 
     let [guayaquil,ambato,tena] = weather_data;
 
-    let {forecast_week} = guayaquil;
+    switch (city_selected) {
+        case "guayaquil":
+          seleccion = guayaquil;
+          console.log(seleccion);
+          break;
+        case "ambato":
+          seleccion = ambato;
+          console.log(seleccion);
+          break;
+        case "tena":
+          seleccion = tena;
+          console.log(seleccion);
+          break;
+    }
+
+    let {forecast_week} = seleccion;
 
     let [dia1,dia2,dia3,dia4,dia5,dia6,dia7]=forecast_week;
 
@@ -104,24 +125,64 @@ let loadWeekForecastData = (a) => {
       
       plantilla_final = plantilla_final + plantilla_semanal;  
 
-      }
+    }
 
      ul.innerHTML= plantilla_final;
 		
 }
 
+let Load_option_ciudades = () => {
+
+    let selector = document.getElementById("dropdownMenuButton")
+    
+    let planilla_ciudades =`<option value="" selected="" disabled="" hidden="">Seleccione una ciudad</option>
+                            <option class="dropdown-item" value="guayaquil">${weather_data[0].city}</option>
+                            <option class="dropdown-item" value="ambato">${weather_data[1].city}</option>
+                            <option class="dropdown-item" value="tena">${weather_data[2].city}</option>`
+
+        selector.innerHTML=planilla_ciudades;
+}
+
+
 
 
 document.addEventListener("DOMContentLoaded", (event) => {
     
-    loadDayForecastData();
+    Load_option_ciudades();
 
 });
+
+
 
 let click_boton= document.getElementById("loadinfo");
 
 click_boton.addEventListener('click', (event) => {
-    
-    loadWeekForecastData();
+
+    if(selectedCity !== "ninguna" )
+    {
+        loadWeekForecastData(selectedCity);
+    } 
+});
+
+
+
+let element_drop = document.getElementById("dropdownMenuButton");
+
+element_drop.addEventListener('change', (event) => {
+   
+   
+    selectedCity = event.target.value ;
+    console.log(selectedCity);
+    Hide_weekForecast();
+    loadDayForecastData(selectedCity);  
 
 });
+
+
+
+let Hide_weekForecast = () =>{
+
+  let element_uls = document.getElementsByTagName("ul");
+  let ul = element_uls[0];
+  ul.innerHTML="";
+}
